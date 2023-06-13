ca6b9c4d6d273b5cd8a0bd620081192f17c02b0e
Will Cockram <wcockram@stark.co.uk>
2023-06-13 14:09:17 +0100

    Add project files.

---
+++ ThemingDemoApp/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ControlzEx.Theming;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ThemingDemoApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            SolidColorBrush blueBrush = new(Colors.Blue);
+
+            Theme light = new("Light.Blue",
+                              "Light Blue",
+                              ThemeManager.BaseColorLight,
+                              "Blue",
+                              Colors.Blue,
+                              blueBrush,
+                              true,
+                              false);
+
+            ThemeManager.Current.AddTheme(light);
+
+            Theme dark = new("Dark.Blue",
+                             "Dark Blue",
+                             ThemeManager.BaseColorDark,
+                             "Blue",
+                             Colors.Blue,
+                             blueBrush,
+                             true,
+                             false);
+
+            ThemeManager.Current.AddTheme(dark);
+
+            ThemeManager.Current.ChangeTheme(this, "Light.Blue");
+        }
+    }
+}
---
+++ ThemingDemoApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ControlzEx.Theming;
+using System.Diagnostics;
+using System.Windows;
+
+namespace ThemingDemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLightTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(this, "Light.Blue");
+            Debug.WriteLine("Changed to Light.Blue");
+        }
+        
+        private void btnDarkTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ThemeManager.Current.ChangeTheme(this, "Dark.Blue");
+            Debug.WriteLine("Changed to Dark.Blue");
+        }
+    }
+}
